2bae0bc02dc35e2d9efb75f3bc879517d6e9e72a
Ilmira Ryakhimova <benameless96@gmail.com>
2025-11-14 01:30:52 +0300

    .

---
+++ backend-service/src/main/java/com/rental/manager/RentalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rental.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagerApplication.class, args);
+	}
+
+}
