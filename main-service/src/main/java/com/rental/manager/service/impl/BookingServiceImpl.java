cc64c07318742b07b44f432c29d3af65d86af9d4
Ilmira Ryakhimova <benameless96@gmail.com>
2025-12-28 16:22:59 +0300

    .

--- main-service/src/main/java/com/rental/manager/service/impl/BookingServiceImpl.java
+++ main-service/src/main/java/com/rental/manager/service/impl/BookingServiceImpl.java
@@ -108,17 +108,17 @@ public class BookingServiceImpl implements BookingService {
                 .orElseThrow(() -> new EntityNotFoundException(BOOKING_NOT_FOUND_MSG + id));
         booking.setBookingStatus(request.getBookingStatus());
         Booking savedBooking = bookingRepository.save(booking);
-        if (request.getBookingStatus() == BookingStatus.CONFIRMED) {
-            String guestEmail = savedBooking.getMainGuest().getEmail();
-            String guestName = savedBooking.getMainGuest().getName();
-            String bookingCode = savedBooking.getBookingCode();
-            emailService.sendBookingInfoToGuest(guestEmail, guestName, bookingCode,
-                    savedBooking.getCheckInDate(), savedBooking.getCheckOutDate());
-            String ownerEmail = savedBooking.getApartment().getOwner().getEmail();
-            String ownerName = savedBooking.getApartment().getOwner().getName();
-            emailService.sendBookingInfoToOwner(ownerEmail, ownerName, guestName, bookingCode,
-                    savedBooking.getCheckInDate(), savedBooking.getCheckOutDate());
-        }
+//        if (request.getBookingStatus() == BookingStatus.CONFIRMED) {
+//            String guestEmail = savedBooking.getMainGuest().getEmail();
+//            String guestName = savedBooking.getMainGuest().getName();
+//            String bookingCode = savedBooking.getBookingCode();
+//            emailService.sendBookingInfoToGuest(guestEmail, guestName, bookingCode,
+//                    savedBooking.getCheckInDate(), savedBooking.getCheckOutDate());
+//            String ownerEmail = savedBooking.getApartment().getOwner().getEmail();
+//            String ownerName = savedBooking.getApartment().getOwner().getName();
+//            emailService.sendBookingInfoToOwner(ownerEmail, ownerName, guestName, bookingCode,
+//                    savedBooking.getCheckInDate(), savedBooking.getCheckOutDate());
+//        }
         return mapper.toDto(savedBooking);
     }
 
