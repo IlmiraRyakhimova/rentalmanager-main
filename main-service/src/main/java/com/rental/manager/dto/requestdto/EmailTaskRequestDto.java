f0c92460ca5f17586da4dd291d3c43327d0568e2
Ilmira Ryakhimova <benameless96@gmail.com>
2025-12-29 19:43:45 +0300

    .

--- main-service/src/main/java/com/rental/manager/dto/requestdto/EmailTaskRequestDto.java
+++ main-service/src/main/java/com/rental/manager/dto/requestdto/EmailTaskRequestDto.java
@@ -1,13 +1,18 @@
 package com.rental.manager.dto.requestdto;
 
 import com.rental.manager.service.queue.EmailTaskType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmailTaskRequestDto implements Serializable {
     private String to;
     private String ownerName;
